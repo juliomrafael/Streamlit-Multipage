36f0b6888a0933d6bc4592c0811c8edac749b160
Júlio Rafael <33831855+jmrafael@users.noreply.github.com>
2022-05-02 19:06:22 +0200

    Update data_stats.py

--- apps/data_stats.py
+++ apps/data_stats.py
@@ -7,7 +7,7 @@ def app():
     st.title('Data Stats')
 
     st.write("This is a sample data stats in the mutliapp.")
-    st.write("See `apps/data_stats.py` to know how to use it.")
+    st.write("See `apps/dataframe.py` to know how to use it.")
 
     st.markdown("### Plot Data")
     df = create_table()
